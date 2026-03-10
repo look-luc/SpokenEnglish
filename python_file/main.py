036536e66afc21a14f48771643124e2c79f2459d
lucdenardi <denardiluc@gmail.com>
2026-03-10 09:26:18 -0600

    getting started on skeleton modle

--- python_file/main.py
+++ python_file/main.py
@@ -1,4 +1,8 @@
+import torch
+
 def main():
+    # checking if there is some kind of GPU available before going to the CPU
+    device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
     return 0
 
 if __name__ == "__main__":
--- python_file/model/is_overlap.py
+++ python_file/model/is_overlap.py
@@ -4,5 +4,8 @@ import torch.nn.functional as F
 
 
 class is_overlap_model(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, input, device, hidden_dim, embedding_dim=300) -> None:
         super(is_overlap_model, self).__init__()
+        self.device = device
+        self.hidden_dim = hidden_dim
+        self.embedding_dim = embedding_dim
