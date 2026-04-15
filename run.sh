d1cd92e045c6bc0d85fd81e87082a2f2b0eed70e
lucdenardi <denardiluc@gmail.com>
2026-04-15 16:52:13 -0600

    added bash file

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=6:00:00
+#SBATCH --output=/projects/%u/multilingual_emotion_tagging/logs/%j.log
+#SBATCH --job-name=spoken_english
+#SBATCH --partition=blanca-clearlab2
+#SBATCH --account=blanca-clearlab2
+#SBATCH --qos=blanca-clearlab2
+#SBATCH --mail-type=END,FAIL
+
+export HF_HOME="/projects/$USER/.cache/huggingface"
+mkdir -p $HF_HOME
+
+set -uo pipefail
+
+REPO_ROOT="/projects/$USER"
+cd "$REPO_ROOT"
+
+module purge
+module load anaconda
+
+set +u && conda activate SpokenEnglish && set -u
+
+cd "$REPO_ROOT/SpokenEnglish/python_file"
+
+python3 main.py
