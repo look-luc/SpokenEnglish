d56a6b5dd1b25031851a4234418ec81b7c77b8f9
lucdenardi <denardiluc@gmail.com>
2026-04-15 14:53:40 -0600

    Resolved merge conflict in .DS_Store

--- python_file/main.py
+++ python_file/main.py
@@ -1,13 +1,14 @@
 import torch
 import torch.nn as nn
 from overlap_task import model
+import pandas as pd
 
 def main():
     # checking if there is some kind of GPU available before going to the CPU
     device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
     vocab_size = 300
     model_discorese = model(vocab_size).to(device)
-    data = []
+    data = pd.read_csv("/data/OverlapProject_TRNOverlapClassifications.tsv")
 
     lr = 0.01
 
